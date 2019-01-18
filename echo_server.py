3867c694d7833e6065ffe1e0227d9f454f5cd983
Pavlo Malynin <pavlo@malynin.ca>
2019-01-17 18:29:36 -0700

    Stuff

--- echo_server.py
+++ echo_server.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 
 import socket
+from multiprocessing import Process
 
 HOST = ""
 PORT = 8001
 BUFFER_SIZE = 1024
+
+def handle_echo(conn, addr):
+    print("Connected by", addr)
+    while True:
+        data = conn.recv(BUFFER_SIZE)
+        if not data: 
+            break
+        conn.send(data)
+    conn.close()
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -12,13 +22,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.listen(1)
     while True:
         conn, addr = s.accept()
-        print("Connected by", addr)
+        p = Process(target=handle_echo, args=(conn, addr))
+        p.daemon = True
+        p.start()
+        print(p)
         
-        while True:
-            data = conn.recv(BUFFER_SIZE)
-            if not data: 
-                break
-            conn.send(data)
-
-        conn.close()
-
--- proxy_server.py
+++ proxy_server.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python
 
 import socket
+import multiprocessing
 
 HOST = ""
 PORT = 8001
 BUFFER_SIZE = 1024
+
+def handle_conn(conn, addr):
+    print("Connected by", addr)
+    with socket.create_connection(("www.google.com", 80)) as goog:
+        while True:
+            data = conn.recv(BUFFER_SIZE)
+            if not data: 
+                break
+            goog.send(data)
+        
+        while True:
+            data = goog.recv(BUFFER_SIZE)
+            if not data: 
+                break
+            conn.send(data)
+
+    conn.close()
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -12,18 +30,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.listen(1)
     while True:
         conn, addr = s.accept()
-        print("Connected by", addr)
-        with socket.create_connection(("www.google.com", 80)) as goog:
-            while True:
-                data = conn.recv(BUFFER_SIZE)
-                if not data: 
-                    break
-                goog.send(data)
-            
-            while True:
-                data = goog.recv(BUFFER_SIZE)
-                if not data: 
-                    break
-                conn.send(data)
-
-        conn.close()
+        p = multiprocessing.Process(target=handle_conn, args=(conn, addr))
+        p.daemon = True
+        p.start()
+        print(p)
+        
